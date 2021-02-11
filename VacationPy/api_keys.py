5d48e202882d7056e4a230c99decfd06beb101c7
Tommy Munson <Tmunson50@MacBook-Air.attlocal.net>
2021-02-10 22:46:02 -0600

    API Homework, I'm exhausted

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "d73b7be5b1d26fc49b27e33db4731fb3"
+
+# Google API Key
+g_key = "AIzaSyDms0KrJnlyqlek0APNk0UbfafvOgRKq8U"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "9146da7edbc909791c42801ae21e289e"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
